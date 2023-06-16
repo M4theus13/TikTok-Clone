6525674a896f688c9e9286bd1fe47b311449416e
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-06-15 22:12:29 -0300

    footer

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react'
+import VideoFooter from './components/footer/VideoFooter'
 import './video.css'
 
 function Video() {
@@ -22,6 +23,7 @@ function Video() {
     <div className='video'>
       <video className='video-player' ref={videoRef} onClick={start} loop src='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z'>
       </video>
+      <VideoFooter/>
     </div>
   )
 }
