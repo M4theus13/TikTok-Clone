67303ef1fa96a1967536f7d857263dc065e3aa47
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-06-16 20:02:24 -0300

    sidebar

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,31 +1,33 @@
 import React, { useRef, useState } from 'react'
 import VideoFooter from './components/footer/VideoFooter'
+import VideoSideBar from './components/sidebar/VideoSideBar'
 import './video.css'
 
-function Video() {
-  
-  const videoRef = useRef(null)
-  const [play, setPlay] = useState(false)
+function Video({likes, messages, shares, name, description, music, url}) {
+    
+    const videoRef = useRef(null)
+    const [play, setPlay] = useState(false)
 
-  function start() {
+    function start() {
 
-    if(play===false) {
-      videoRef.current.play()
-      setPlay(true)
-    } else {
-      videoRef.current.pause()
-      setPlay(false)
+        if(play===false) {
+        videoRef.current.play()
+        setPlay(true)
+        } else {
+        videoRef.current.pause()
+        setPlay(false)
+        }
+
     }
 
-  }
-
-  return (
-    <div className='video'>
-      <video className='video-player' ref={videoRef} onClick={start} loop src='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z'>
-      </video>
-      <VideoFooter/>
-    </div>
-  )
+    return (
+        <div className='video'>
+        <video className='video-player' ref={videoRef} onClick={start} loop src={url}>
+        </video>
+        <VideoSideBar likes={likes} messages={messages} shares={shares}/>
+        <VideoFooter name={name} description={description} music={music}/>
+        </div>
+    )
 }
 
 export default Video
